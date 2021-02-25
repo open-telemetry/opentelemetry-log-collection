d8dd0e98a834a031d658778a16a996e85be3e770
wph95 <justwph@gmail.com>
2021-02-25 22:14:10 +0800

    fix small lint problem

--- entry/field.go
+++ entry/field.go
@@ -184,7 +184,7 @@ func splitField(s string) ([]string, error) {
 		fields = append(fields, s[tokenStart:])
 	}
 
-	if len(fields)==0 {
+	if len(fields) == 0 {
 		return nil, fmt.Errorf("fields size is 0")
 	}
 
