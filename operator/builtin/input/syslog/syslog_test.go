8398d0adde3fb88eb76b0b00c11c9abee9a476e1
Dan Jaglowski <dan.jaglowski@bluemedora.com>
2021-03-01 16:10:24 -0500

    Add copyright to files added in PR 24

--- operator/builtin/input/syslog/syslog_test.go
+++ operator/builtin/input/syslog/syslog_test.go
@@ -1,16 +1,31 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package syslog
 
 import (
 	"fmt"
+	"net"
+	"testing"
+	"time"
+
 	"github.com/open-telemetry/opentelemetry-log-collection/operator/builtin/input/tcp"
 	"github.com/open-telemetry/opentelemetry-log-collection/operator/builtin/input/udp"
 	"github.com/open-telemetry/opentelemetry-log-collection/operator/builtin/parser/syslog"
 	"github.com/open-telemetry/opentelemetry-log-collection/pipeline"
 	"github.com/open-telemetry/opentelemetry-log-collection/testutil"
 	"github.com/stretchr/testify/require"
-	"net"
-	"testing"
-	"time"
 )
 
 func TestSyslogInput(t *testing.T) {
@@ -53,7 +68,6 @@ func SyslogInputTest(t *testing.T, cfg *SyslogInputConfig, tc syslog.Case) {
 		conn, err = net.Dial("udp", cfg.Udp.ListenAddress)
 		require.NoError(t, err)
 	}
-
 
 	switch tc.InputRecord.(type) {
 	case string:
