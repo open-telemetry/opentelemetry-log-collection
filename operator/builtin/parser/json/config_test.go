239be9fbed551861c0d3532ab40486fdb3048bf1
Mrod1598 <miguelrodriguez1598@gmail.com>
2021-03-10 12:46:17 -0500

    Add basic structurefor testing json parser unmarshalling

---
+++ operator/builtin/parser/json/config_test.go
@@ -0,0 +1,112 @@
+package json
+
+import (
+	"fmt"
+	"io/ioutil"
+	"path"
+	"testing"
+
+	"github.com/mitchellh/mapstructure"
+	"github.com/open-telemetry/opentelemetry-log-collection/entry"
+	"github.com/open-telemetry/opentelemetry-log-collection/operator/helper"
+	"github.com/stretchr/testify/require"
+	"gopkg.in/yaml.v2"
+)
+
+type testCase struct {
+	name      string
+	expectErr bool
+	expect    *JSONParserConfig
+}
+
+func TestConfig(t *testing.T) {
+	cases := []testCase{
+		{
+			"default",
+			false,
+			defaultCfg(),
+		},
+		{
+			"parse from simple",
+			false,
+			func() *JSONParserConfig {
+				cfg := defaultCfg()
+				cfg.ParseFrom = entry.NewRecordField("log")
+				return cfg
+			}(),
+		},
+		{
+			"parse to simple",
+			false,
+			func() *JSONParserConfig {
+				cfg := defaultCfg()
+				cfg.ParseTo = entry.NewRecordField("log")
+				return cfg
+			}(),
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run("yaml/"+tc.name, func(t *testing.T) {
+			cfgFromYaml, yamlErr := configFromFileViaYaml(t, path.Join(".", "testdata", fmt.Sprintf("%s.yaml", tc.name)))
+			if tc.expectErr {
+				require.Error(t, yamlErr)
+			} else {
+				require.NoError(t, yamlErr)
+				require.Equal(t, tc.expect, cfgFromYaml)
+			}
+		})
+		// t.Run("mapstructure/"+tc.name, func(t *testing.T) {
+		// 	cfgFromMapstructure, mapErr := configFromFileViaMapstructure(path.Join(".", "testdata", fmt.Sprintf("%s.yaml", tc.name)))
+		// 	if tc.expectErr {
+		// 		require.Error(t, mapErr)
+		// 	} else {
+		// 		require.NoError(t, mapErr)
+		// 		require.Equal(t, tc.expect, cfgFromMapstructure)
+		// 	}
+		// })
+	}
+}
+
+func configFromFileViaYaml(t *testing.T, file string) (*JSONParserConfig, error) {
+	bytes, err := ioutil.ReadFile(file)
+	if err != nil {
+		return nil, fmt.Errorf("could not find config file: %s", err)
+	}
+
+	config := NewJSONParserConfig("json_parser")
+	if err := yaml.Unmarshal(bytes, config); err != nil {
+		return nil, fmt.Errorf("failed to read config file as yaml: %s", err)
+	}
+
+	return config, nil
+}
+
+func configFromFileViaMapstructure(file string) (*JSONParserConfig, error) {
+	bytes, err := ioutil.ReadFile(file)
+	if err != nil {
+		return nil, fmt.Errorf("could not find config file: %s", err)
+	}
+
+	raw := map[string]interface{}{}
+
+	if err := yaml.Unmarshal(bytes, raw); err != nil {
+		return nil, fmt.Errorf("failed to read data from yaml: %s", err)
+	}
+
+	cfg := defaultCfg()
+	dc := &mapstructure.DecoderConfig{Result: cfg, DecodeHook: helper.JSONUnmarshalerHook()}
+	ms, err := mapstructure.NewDecoder(dc)
+	if err != nil {
+		return nil, err
+	}
+	err = ms.Decode(raw)
+	if err != nil {
+		return nil, err
+	}
+	return cfg, nil
+}
+
+func defaultCfg() *JSONParserConfig {
+	return NewJSONParserConfig("json_parser")
+}
