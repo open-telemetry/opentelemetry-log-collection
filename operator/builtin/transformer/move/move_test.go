8f293664cd765c4a75aad2a6aaf47d38e55b601d
Mrod1598 <miguelrodriguez1598@gmail.com>
2021-03-24 16:37:51 -0400

    Add golden config testing to time (#72)

---
+++ operator/builtin/transformer/move/move_test.go
@@ -0,0 +1,93 @@
+package move
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/open-telemetry/opentelemetry-log-collection/entry"
+	"github.com/open-telemetry/opentelemetry-log-collection/operator"
+	"github.com/open-telemetry/opentelemetry-log-collection/testutil"
+	"github.com/stretchr/testify/require"
+)
+
+func TestMoveOperator(t *testing.T) {
+
+	newTestEntry := func() *entry.Entry {
+		e := entry.New()
+		e.Timestamp = time.Unix(1586632809, 0)
+		e.Record = map[string]interface{}{
+			"key": "val",
+			"nested": map[string]interface{}{
+				"nestedkey": "nestedval",
+			},
+		}
+		return e
+	}
+
+	cases := []struct {
+		name      string
+		addOp     MoveOperator
+		input     *entry.Entry
+		output    *entry.Entry
+		expectErr bool
+	}{
+		{
+			name: "MoveValue",
+			addOp: MoveOperator{
+				From: entry.NewRecordField("key"),
+				To:   entry.NewRecordField("new"),
+			},
+			input: newTestEntry(),
+			output: func() *entry.Entry {
+				e := newTestEntry()
+				e.Record = map[string]interface{}{
+					"new": "val",
+					"nested": map[string]interface{}{
+						"nestedkey": "nestedval",
+					},
+				}
+				return e
+			}(),
+		},
+		{
+			name: "MoveNest",
+			addOp: MoveOperator{
+				From: entry.NewRecordField("nested"),
+				To:   entry.NewRecordField("NewNested"),
+			},
+			input: newTestEntry(),
+			output: func() *entry.Entry {
+				e := newTestEntry()
+				e.Record = map[string]interface{}{
+					"key": "val",
+					"NewNested": map[string]interface{}{
+						"nestedkey": "nestedval",
+					},
+				}
+				return e
+			}(),
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			cfg := NewMoveOperatorConfig("test")
+			cfg.To = tc.addOp.To
+			cfg.From = tc.addOp.From
+			cfg.OutputIDs = []string{"fake"}
+			ops, err := cfg.Build(testutil.NewBuildContext(t))
+			require.NoError(t, err)
+			op := ops[0]
+
+			add := op.(*MoveOperator)
+			fake := testutil.NewFakeOutput(t)
+			add.SetOutputs([]operator.Operator{fake})
+
+			err = add.Process(context.Background(), tc.input)
+			require.NoError(t, err)
+
+			fake.ExpectEntry(t, tc.output)
+		})
+	}
+}
