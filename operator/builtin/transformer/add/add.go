67b7624b01a50fef2672b2a99dd1bc7c723d2843
Mrod1598 <miguelrodriguez1598@gmail.com>
2021-03-29 09:32:34 -0400

    Add new 'Add' operator

---
+++ operator/builtin/transformer/add/add.go
@@ -0,0 +1,76 @@
+package add
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/antonmedv/expr/vm"
+	"github.com/open-telemetry/opentelemetry-log-collection/entry"
+	"github.com/open-telemetry/opentelemetry-log-collection/operator"
+	"github.com/open-telemetry/opentelemetry-log-collection/operator/helper"
+)
+
+func init() {
+	operator.Register("add", func() operator.Builder { return NewAddOperatorConfig("") })
+}
+
+// NewAddOperatorConfig creates a new restructure operator config with default values
+func NewAddOperatorConfig(operatorID string) *AddOperatorConfig {
+	return &AddOperatorConfig{
+		TransformerConfig: helper.NewTransformerConfig(operatorID, "add"),
+	}
+}
+
+// AddOperatorConfig is the configuration of a restructure operator
+type AddOperatorConfig struct {
+	helper.TransformerConfig `mapstructure:",squash" yaml:",inline"`
+	Field                    entry.Field `json:"field" yaml:"field"`
+	Value                    interface{} `json:"value,omitempty" yaml:"value,omitempty"`
+	program                  *vm.Program
+}
+
+func (c AddOperatorConfig) Build(context operator.BuildContext) ([]operator.Operator, error) {
+	transformerOperator, err := c.TransformerConfig.Build(context)
+	if err != nil {
+		return nil, err
+	}
+
+	addOperator := &AddOperator{
+		TransformerOperator: transformerOperator,
+		Field:               c.Field,
+		program:             c.program,
+		Value:               c.Value,
+	}
+
+	return []operator.Operator{addOperator}, nil
+}
+
+type AddOperator struct {
+	helper.TransformerOperator `mapstructure:",squash" yaml:",inline"`
+
+	Field   entry.Field `json:"field" yaml:"field"`
+	Value   interface{} `json:"value,omitempty" yaml:"value,omitempty"`
+	program *vm.Program
+}
+
+// Process will process an entry with a restructure transformation.
+func (p *AddOperator) Process(ctx context.Context, entry *entry.Entry) error {
+	return p.ProcessWith(ctx, entry, p.Transform)
+}
+
+// Transform will apply the restructure operations to an entry
+func (p *AddOperator) Transform(entry *entry.Entry) error {
+	if p.Value != nil {
+		return entry.Set(p.Field, p.Value)
+	} else if p.program != nil {
+		env := helper.GetExprEnv(entry)
+		defer helper.PutExprEnv(env)
+
+		result, err := vm.Run(p.program, env)
+		if err != nil {
+			return fmt.Errorf("evaluate value_expr: %s", err)
+		}
+		return entry.Set(p.Field, result)
+	}
+	return fmt.Errorf("add: missing required field 'Value'")
+}
---
+++ operator/builtin/transformer/add/add_test.go
@@ -0,0 +1,112 @@
+package add
+
+import (
+	"context"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/antonmedv/expr"
+	"github.com/antonmedv/expr/vm"
+	"github.com/open-telemetry/opentelemetry-log-collection/entry"
+	"github.com/open-telemetry/opentelemetry-log-collection/operator"
+	"github.com/open-telemetry/opentelemetry-log-collection/testutil"
+	"github.com/stretchr/testify/require"
+)
+
+func TestAddOperator(t *testing.T) {
+	os.Setenv("TEST_ADD_PLUGIN_ENV", "foo")
+	defer os.Unsetenv("TEST_ADD_PLUGIN_ENV")
+
+	newTestEntry := func() *entry.Entry {
+		e := entry.New()
+		e.Timestamp = time.Unix(1586632809, 0)
+		e.Record = map[string]interface{}{
+			"key": "val",
+			"nested": map[string]interface{}{
+				"nestedkey": "nestedval",
+			},
+		}
+		return e
+	}
+
+	cases := []struct {
+		name      string
+		addOp     AddOperator
+		input     *entry.Entry
+		output    *entry.Entry
+		expectErr bool
+	}{
+		{
+			name: "AddValue",
+			addOp: AddOperator{
+				Field: entry.NewRecordField("new"),
+				Value: "randomMessage",
+			},
+			input: newTestEntry(),
+			output: func() *entry.Entry {
+				e := newTestEntry()
+				e.Record.(map[string]interface{})["new"] = "randomMessage"
+				return e
+			}(),
+		},
+		{
+			name: "add_expr",
+			addOp: AddOperator{
+				Field: entry.NewRecordField("new"),
+				program: func() *vm.Program {
+					vm, err := expr.Compile(`$.key + "_suffix"`)
+					require.NoError(t, err)
+					return vm
+				}(),
+			},
+			input: newTestEntry(),
+			output: func() *entry.Entry {
+				e := newTestEntry()
+				e.Record.(map[string]interface{})["new"] = "val_suffix"
+				return e
+			}(),
+			expectErr: false,
+		},
+		{
+			name: "add_expr",
+			addOp: AddOperator{
+				Field: entry.NewRecordField("new"),
+				program: func() *vm.Program {
+					vm, err := expr.Compile(`env("TEST_ADD_PLUGIN_ENV")`)
+					require.NoError(t, err)
+					return vm
+				}(),
+			},
+			input: newTestEntry(),
+			output: func() *entry.Entry {
+				e := newTestEntry()
+				e.Record.(map[string]interface{})["new"] = "foo"
+				return e
+			}(),
+			expectErr: false,
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			cfg := NewAddOperatorConfig("test")
+			cfg.Field = tc.addOp.Field
+			cfg.Value = tc.addOp.Value
+			cfg.program = tc.addOp.program
+			cfg.OutputIDs = []string{"fake"}
+			ops, err := cfg.Build(testutil.NewBuildContext(t))
+			require.NoError(t, err)
+			op := ops[0]
+
+			add := op.(*AddOperator)
+			fake := testutil.NewFakeOutput(t)
+			add.SetOutputs([]operator.Operator{fake})
+
+			err = add.Process(context.Background(), tc.input)
+			require.NoError(t, err)
+
+			fake.ExpectEntry(t, tc.output)
+		})
+	}
+}
