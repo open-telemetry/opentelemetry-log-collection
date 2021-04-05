ca828a9e442fbdd3fce4c85f552f8cf1035d92c1
Mrod1598 <miguelrodriguez1598@gmail.com>
2021-04-05 13:09:17 -0400

    Add end to end testing

--- operator/builtin/transformer/retain/retain_test.go
+++ operator/builtin/transformer/retain/retain_test.go
@@ -1,3 +1,5 @@
+package retain
+
 // Copyright The OpenTelemetry Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,20 +14,31 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package retain
-
 import (
 	"context"
+	"fmt"
+	"io/ioutil"
+	"path"
 	"testing"
 	"time"
 
+	"github.com/stretchr/testify/require"
+	"gopkg.in/yaml.v2"
+
 	"github.com/open-telemetry/opentelemetry-log-collection/entry"
 	"github.com/open-telemetry/opentelemetry-log-collection/operator"
+	"github.com/open-telemetry/opentelemetry-log-collection/operator/helper"
 	"github.com/open-telemetry/opentelemetry-log-collection/testutil"
-	"github.com/stretchr/testify/require"
 )
 
-func TestOperator(t *testing.T) {
+type testCase struct {
+	name      string
+	expectErr bool
+	input     func() *entry.Entry
+	output    func() *entry.Entry
+}
+
+func TestRetainGoldenConfig(t *testing.T) {
 	newTestEntry := func() *entry.Entry {
 		e := entry.New()
 		e.Timestamp = time.Unix(1586632809, 0)
@@ -38,36 +51,23 @@ func TestOperator(t *testing.T) {
 		return e
 	}
 
-	cases := []struct {
-		name      string
-		retainOp  RetainOperator
-		input     *entry.Entry
-		output    *entry.Entry
-		expectErr bool
-	}{
+	cases := []testCase{
 		{
-			name: "retain_single",
-			retainOp: RetainOperator{
-				Fields: []entry.Field{entry.NewRecordField("key")},
-			},
-			input: newTestEntry(),
-			output: func() *entry.Entry {
+			"retain_single",
+			false,
+			newTestEntry,
+			func() *entry.Entry {
 				e := newTestEntry()
 				e.Record = map[string]interface{}{
 					"key": "val",
 				}
 				return e
-			}(),
+			},
 		},
 		{
-			name: "retain_multi",
-			retainOp: RetainOperator{
-				Fields: []entry.Field{
-					entry.NewRecordField("key"),
-					entry.NewRecordField("nested2"),
-				},
-			},
-			input: func() *entry.Entry {
+			"retain_multi",
+			false,
+			func() *entry.Entry {
 				e := newTestEntry()
 				e.Record = map[string]interface{}{
 					"key": "val",
@@ -79,8 +79,8 @@ func TestOperator(t *testing.T) {
 					},
 				}
 				return e
-			}(),
-			output: func() *entry.Entry {
+			},
+			func() *entry.Entry {
 				e := newTestEntry()
 				e.Record = map[string]interface{}{
 					"key": "val",
@@ -89,40 +89,31 @@ func TestOperator(t *testing.T) {
 					},
 				}
 				return e
-			}(),
+			},
 		},
 		{
-			name: "retain_single_attribute",
-			retainOp: RetainOperator{
-				Fields: []entry.Field{
-					entry.NewAttributeField("key"),
-				},
-			},
-			input: func() *entry.Entry {
+			"retain_single_attribute",
+			false,
+			func() *entry.Entry {
 				e := newTestEntry()
 				e.Attributes = map[string]string{
 					"key": "val",
 				}
 				return e
-			}(),
-			output: func() *entry.Entry {
+			},
+			func() *entry.Entry {
 				e := newTestEntry()
 				e.Attributes = map[string]string{
 					"key": "val",
 				}
 				e.Record = nil
 				return e
-			}(),
+			},
 		},
 		{
-			name: "retain_multi_attribute",
-			retainOp: RetainOperator{
-				Fields: []entry.Field{
-					entry.NewAttributeField("key1"),
-					entry.NewAttributeField("key2"),
-				},
-			},
-			input: func() *entry.Entry {
+			"retain_multi_attribute",
+			false,
+			func() *entry.Entry {
 				e := newTestEntry()
 				e.Attributes = map[string]string{
 					"key1": "val",
@@ -130,8 +121,8 @@ func TestOperator(t *testing.T) {
 					"key3": "val",
 				}
 				return e
-			}(),
-			output: func() *entry.Entry {
+			},
+			func() *entry.Entry {
 				e := newTestEntry()
 				e.Attributes = map[string]string{
 					"key1": "val",
@@ -139,40 +130,53 @@ func TestOperator(t *testing.T) {
 				}
 				e.Record = nil
 				return e
-			}(),
+			},
 		},
 		{
-			name: "retain_single_resource",
-			retainOp: RetainOperator{
-				Fields: []entry.Field{
-					entry.NewResourceField("key"),
-				},
+			"retain_multi_attribute",
+			false,
+			func() *entry.Entry {
+				e := newTestEntry()
+				e.Attributes = map[string]string{
+					"key1": "val",
+					"key2": "val",
+					"key3": "val",
+				}
+				return e
 			},
-			input: func() *entry.Entry {
+			func() *entry.Entry {
+				e := newTestEntry()
+				e.Attributes = map[string]string{
+					"key1": "val",
+					"key2": "val",
+				}
+				e.Record = nil
+				return e
+			},
+		},
+		{
+			"retain_single_resource",
+			false,
+			func() *entry.Entry {
 				e := newTestEntry()
 				e.Resource = map[string]string{
 					"key": "val",
 				}
 				return e
-			}(),
-			output: func() *entry.Entry {
+			},
+			func() *entry.Entry {
 				e := newTestEntry()
 				e.Resource = map[string]string{
 					"key": "val",
 				}
 				e.Record = nil
 				return e
-			}(),
+			},
 		},
 		{
-			name: "retain_multi_resource",
-			retainOp: RetainOperator{
-				Fields: []entry.Field{
-					entry.NewResourceField("key1"),
-					entry.NewResourceField("key2"),
-				},
-			},
-			input: func() *entry.Entry {
+			"retain_multi_resource",
+			false,
+			func() *entry.Entry {
 				e := newTestEntry()
 				e.Resource = map[string]string{
 					"key1": "val",
@@ -180,8 +184,8 @@ func TestOperator(t *testing.T) {
 					"key3": "val",
 				}
 				return e
-			}(),
-			output: func() *entry.Entry {
+			},
+			func() *entry.Entry {
 				e := newTestEntry()
 				e.Resource = map[string]string{
 					"key1": "val",
@@ -189,18 +193,12 @@ func TestOperator(t *testing.T) {
 				}
 				e.Record = nil
 				return e
-			}(),
+			},
 		},
 		{
-			name: "retain_one_of_each",
-			retainOp: RetainOperator{
-				Fields: []entry.Field{
-					entry.NewRecordField("key"),
-					entry.NewResourceField("key1"),
-					entry.NewAttributeField("key3"),
-				},
-			},
-			input: func() *entry.Entry {
+			"retain_one_of_each",
+			false,
+			func() *entry.Entry {
 				e := newTestEntry()
 				e.Resource = map[string]string{
 					"key1": "val",
@@ -211,8 +209,8 @@ func TestOperator(t *testing.T) {
 					"key4": "val",
 				}
 				return e
-			}(),
-			output: func() *entry.Entry {
+			},
+			func() *entry.Entry {
 				e := newTestEntry()
 				e.Resource = map[string]string{
 					"key1": "val",
@@ -224,27 +222,87 @@ func TestOperator(t *testing.T) {
 					"key": "val",
 				}
 				return e
-			}(),
+			},
 		},
 	}
-
 	for _, tc := range cases {
-		t.Run(tc.name, func(t *testing.T) {
-			cfg := NewRetainOperatorConfig("test")
-			cfg.Fields = tc.retainOp.Fields
-			cfg.OutputIDs = []string{"fake"}
-			ops, err := cfg.Build(testutil.NewBuildContext(t))
+		t.Run("yaml/"+tc.name, func(t *testing.T) {
+			cfgFromYaml, _ := configFromFileViaYaml(path.Join(".", "testdata", fmt.Sprintf("%s.yaml", tc.name)))
+			cfgFromYaml.OutputIDs = []string{"fake"}
+			cfgFromYaml.OnError = "drop"
+			ops, err := cfgFromYaml.Build(testutil.NewBuildContext(t))
 			require.NoError(t, err)
 			op := ops[0]
 
-			add := op.(*RetainOperator)
+			remove := op.(*RetainOperator)
 			fake := testutil.NewFakeOutput(t)
-			add.SetOutputs([]operator.Operator{fake})
+			remove.SetOutputs([]operator.Operator{fake})
 
-			err = add.Process(context.Background(), tc.input)
+			err = remove.Process(context.Background(), tc.input())
+			if tc.expectErr {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+				fake.ExpectEntry(t, tc.output())
+			}
+		})
+		t.Run("mapstructure/"+tc.name, func(t *testing.T) {
+			cfgFromMapstructure := defaultCfg()
+			configFromFileViaMapstructure(
+				path.Join(".", "testdata", fmt.Sprintf("%s.yaml", tc.name)),
+				cfgFromMapstructure,
+			)
+			cfgFromMapstructure.OutputIDs = []string{"fake"}
+			cfgFromMapstructure.OnError = "drop"
+			ops, err := cfgFromMapstructure.Build(testutil.NewBuildContext(t))
 			require.NoError(t, err)
+			op := ops[0]
 
-			fake.ExpectEntry(t, tc.output)
+			remove := op.(*RetainOperator)
+			fake := testutil.NewFakeOutput(t)
+			remove.SetOutputs([]operator.Operator{fake})
+			val := tc.input()
+			err = remove.Process(context.Background(), val)
+			if tc.expectErr {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+				fake.ExpectEntry(t, tc.output())
+			}
 		})
 	}
+}
+
+func configFromFileViaYaml(file string) (*RetainOperatorConfig, error) {
+	bytes, err := ioutil.ReadFile(file)
+	if err != nil {
+		return nil, fmt.Errorf("could not find config file: %s", err)
+	}
+
+	config := defaultCfg()
+	if err := yaml.Unmarshal(bytes, config); err != nil {
+		return nil, fmt.Errorf("failed to read config file as yaml: %s", err)
+	}
+
+	return config, nil
+}
+
+func configFromFileViaMapstructure(file string, result *RetainOperatorConfig) error {
+	bytes, err := ioutil.ReadFile(file)
+	if err != nil {
+		return fmt.Errorf("could not find config file: %s", err)
+	}
+
+	raw := map[string]interface{}{}
+
+	if err := yaml.Unmarshal(bytes, raw); err != nil {
+		return fmt.Errorf("failed to read data from yaml: %s", err)
+	}
+
+	err = helper.UnmarshalMapstructure(raw, result)
+	return err
+}
+
+func defaultCfg() *RetainOperatorConfig {
+	return NewRetainOperatorConfig("remove")
 }
