374482a6e729f21803c0469b112a5f88c870e951
Mrod1598 <miguelrodriguez1598@gmail.com>
2021-04-05 11:04:41 -0400

    Fix imports

--- operator/builtin/transformer/flatten/flatten_test.go
+++ operator/builtin/transformer/flatten/flatten_test.go
@@ -8,12 +8,13 @@ import (
 	"testing"
 	"time"
 
+	"github.com/stretchr/testify/require"
+	"gopkg.in/yaml.v2"
+
 	"github.com/open-telemetry/opentelemetry-log-collection/entry"
 	"github.com/open-telemetry/opentelemetry-log-collection/operator"
 	"github.com/open-telemetry/opentelemetry-log-collection/operator/helper"
 	"github.com/open-telemetry/opentelemetry-log-collection/testutil"
-	"github.com/stretchr/testify/require"
-	"gopkg.in/yaml.v2"
 )
 
 type testCase struct {
