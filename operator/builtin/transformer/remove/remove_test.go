dd3d3a5dac4c29d6b5ae43707133e915a74b293b
Mrod1598 <miguelrodriguez1598@gmail.com>
2021-04-07 14:04:56 -0400

    Change testing

--- operator/builtin/transformer/remove/remove_test.go
+++ operator/builtin/transformer/remove/remove_test.go
@@ -1,34 +1,43 @@
+// // Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package remove
 
 import (
 	"context"
-	"fmt"
-	"io/ioutil"
-	"path"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/require"
-	yaml "gopkg.in/yaml.v2"
 
 	"github.com/open-telemetry/opentelemetry-log-collection/entry"
 	"github.com/open-telemetry/opentelemetry-log-collection/operator"
-	"github.com/open-telemetry/opentelemetry-log-collection/operator/helper"
 	"github.com/open-telemetry/opentelemetry-log-collection/testutil"
 )
 
 type testCase struct {
 	name      string
-	expectErr bool
+	op        *RemoveOperatorConfig
 	input     func() *entry.Entry
 	output    func() *entry.Entry
+	expectErr bool
 }
 
-func TestRemoveGoldenConfig(t *testing.T) {
+func TestAddGoldenConfig(t *testing.T) {
 	newTestEntry := func() *entry.Entry {
 		e := entry.New()
 		e.Timestamp = time.Unix(1586632809, 0)
-		e.Record = map[string]interface{}{
+		e.Body = map[string]interface{}{
 			"key": "val",
 			"nested": map[string]interface{}{
 				"nestedkey": "nestedval",
@@ -40,7 +49,9 @@ func TestRemoveGoldenConfig(t *testing.T) {
 	cases := []testCase{
 		{
 			"remove_one",
-			false,
+			func() *RemoveOperatorConfig {
+				cfg := 
+			},
 			newTestEntry,
 			func() *entry.Entry {
 				e := newTestEntry()
@@ -49,6 +60,7 @@ func TestRemoveGoldenConfig(t *testing.T) {
 				}
 				return e
 			},
+			false,
 		},
 		{
 			"remove_multi",
@@ -127,91 +139,26 @@ func TestRemoveGoldenConfig(t *testing.T) {
 			},
 		},
 	}
-
 	for _, tc := range cases {
-		t.Run("yaml/"+tc.name, func(t *testing.T) {
-			cfgFromYaml, yamlErr := configFromFileViaYaml(path.Join(".", "testdata", fmt.Sprintf("%s.yaml", tc.name)))
+		t.Run("BuildandProcess/"+tc.name, func(t *testing.T) {
+			cfgFromMapstructure := tc.op
+			cfgFromMapstructure.OutputIDs = []string{"fake"}
+			cfgFromMapstructure.OnError = "drop"
+			ops, err := cfgFromMapstructure.Build(testutil.NewBuildContext(t))
+			require.NoError(t, err)
+			op := ops[0]
+
+			add := op.(*AddOperator)
+			fake := testutil.NewFakeOutput(t)
+			add.SetOutputs([]operator.Operator{fake})
+			val := tc.input()
+			err = add.Process(context.Background(), val)
 			if tc.expectErr {
-				require.Error(t, yamlErr)
+				require.Error(t, err)
 			} else {
-				cfgFromYaml.OutputIDs = []string{"fake"}
-				ops, err := cfgFromYaml.Build(testutil.NewBuildContext(t))
 				require.NoError(t, err)
-				op := ops[0]
-
-				remove := op.(*RemoveOperator)
-				fake := testutil.NewFakeOutput(t)
-				remove.SetOutputs([]operator.Operator{fake})
-
-				err = remove.Process(context.Background(), tc.input())
-				if tc.expectErr {
-					require.Error(t, err)
-				}
-				require.NoError(t, err)
-
-				fake.ExpectEntry(t, tc.output())
-			}
-		})
-		t.Run("mapstructure/"+tc.name, func(t *testing.T) {
-			cfgFromMapstructure := defaultCfg()
-			mapErr := configFromFileViaMapstructure(
-				path.Join(".", "testdata", fmt.Sprintf("%s.yaml", tc.name)),
-				cfgFromMapstructure,
-			)
-			if tc.expectErr {
-				require.Error(t, mapErr)
-			} else {
-				cfgFromMapstructure.OutputIDs = []string{"fake"}
-				ops, err := cfgFromMapstructure.Build(testutil.NewBuildContext(t))
-				require.NoError(t, err)
-				op := ops[0]
-
-				remove := op.(*RemoveOperator)
-				fake := testutil.NewFakeOutput(t)
-				remove.SetOutputs([]operator.Operator{fake})
-
-				err = remove.Process(context.Background(), tc.input())
-				if tc.expectErr {
-					require.Error(t, err)
-				}
-				require.NoError(t, err)
-
 				fake.ExpectEntry(t, tc.output())
 			}
 		})
 	}
-}
-
-func configFromFileViaYaml(file string) (*RemoveOperatorConfig, error) {
-	bytes, err := ioutil.ReadFile(file)
-	if err != nil {
-		return nil, fmt.Errorf("could not find config file: %s", err)
-	}
-
-	config := defaultCfg()
-	if err := yaml.Unmarshal(bytes, config); err != nil {
-		return nil, fmt.Errorf("failed to read config file as yaml: %s", err)
-	}
-
-	return config, nil
-}
-
-func configFromFileViaMapstructure(file string, result *RemoveOperatorConfig) error {
-	bytes, err := ioutil.ReadFile(file)
-	if err != nil {
-		return fmt.Errorf("could not find config file: %s", err)
-	}
-
-	raw := map[string]interface{}{}
-
-	if err := yaml.Unmarshal(bytes, raw); err != nil {
-		return fmt.Errorf("failed to read data from yaml: %s", err)
-	}
-
-	err = helper.UnmarshalMapstructure(raw, result)
-	return err
-}
-
-func defaultCfg() *RemoveOperatorConfig {
-	return NewRemoveOperatorConfig("remove")
 }
