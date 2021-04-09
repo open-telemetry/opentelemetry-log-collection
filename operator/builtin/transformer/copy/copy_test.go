4a0ffc8e8ca3d6cc3860e8fa43df2ae6dc589a0c
Mrod1598 <miguelrodriguez1598@gmail.com>
2021-04-09 13:06:16 -0400

    Update tests

--- operator/builtin/transformer/copy/copy_test.go
+++ operator/builtin/transformer/copy/copy_test.go
@@ -15,33 +15,29 @@ package copy
 
 import (
 	"context"
-	"fmt"
-	"io/ioutil"
-	"path"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/require"
-	"gopkg.in/yaml.v2"
 
 	"github.com/open-telemetry/opentelemetry-log-collection/entry"
 	"github.com/open-telemetry/opentelemetry-log-collection/operator"
-	"github.com/open-telemetry/opentelemetry-log-collection/operator/helper"
 	"github.com/open-telemetry/opentelemetry-log-collection/testutil"
 )
 
 type testCase struct {
 	name      string
 	expectErr bool
-	input     *entry.Entry
-	output    *entry.Entry
+	op        *CopyOperatorConfig
+	input     func() *entry.Entry
+	output    func() *entry.Entry
 }
 
-func TestCopyGoldenConfig(t *testing.T) {
+func TestBuildAndProcess(t *testing.T) {
 	newTestEntry := func() *entry.Entry {
 		e := entry.New()
 		e.Timestamp = time.Unix(1586632809, 0)
-		e.Record = map[string]interface{}{
+		e.Body = map[string]interface{}{
 			"key": "val",
 			"nested": map[string]interface{}{
 				"nestedkey": "nestedval",
@@ -52,12 +48,18 @@ func TestCopyGoldenConfig(t *testing.T) {
 
 	cases := []testCase{
 		{
-			"record_to_record",
+			"body_to_body",
 			false,
-			newTestEntry(),
+			func() *CopyOperatorConfig {
+				cfg := defaultCfg()
+				cfg.From = entry.NewBodyField("key")
+				cfg.To = entry.NewBodyField("key2")
+				return cfg
+			}(),
+			newTestEntry,
 			func() *entry.Entry {
 				e := newTestEntry()
-				e.Record = map[string]interface{}{
+				e.Body = map[string]interface{}{
 					"key": "val",
 					"nested": map[string]interface{}{
 						"nestedkey": "nestedval",
@@ -65,15 +67,65 @@ func TestCopyGoldenConfig(t *testing.T) {
 					"key2": "val",
 				}
 				return e
-			}(),
+			},
 		},
 		{
-			"record_to_attribute",
+			"nested_to_body",
 			false,
-			newTestEntry(),
+			func() *CopyOperatorConfig {
+				cfg := defaultCfg()
+				cfg.From = entry.NewBodyField("nested", "nestedkey")
+				cfg.To = entry.NewBodyField("key2")
+				return cfg
+			}(),
+			newTestEntry,
 			func() *entry.Entry {
 				e := newTestEntry()
-				e.Record = map[string]interface{}{
+				e.Body = map[string]interface{}{
+					"key": "val",
+					"nested": map[string]interface{}{
+						"nestedkey": "nestedval",
+					},
+					"key2": "nestedval",
+				}
+				return e
+			},
+		},
+		{
+			"body_to_nested",
+			false,
+			func() *CopyOperatorConfig {
+				cfg := defaultCfg()
+				cfg.From = entry.NewBodyField("key")
+				cfg.To = entry.NewBodyField("nested", "key2")
+				return cfg
+			}(),
+			newTestEntry,
+			func() *entry.Entry {
+				e := newTestEntry()
+				e.Body = map[string]interface{}{
+					"key": "val",
+					"nested": map[string]interface{}{
+						"nestedkey": "nestedval",
+						"key2":      "val",
+					},
+				}
+				return e
+			},
+		},
+		{
+			"body_to_attribute",
+			false,
+			func() *CopyOperatorConfig {
+				cfg := defaultCfg()
+				cfg.From = entry.NewBodyField("key")
+				cfg.To = entry.NewAttributeField("key2")
+				return cfg
+			}(),
+			newTestEntry,
+			func() *entry.Entry {
+				e := newTestEntry()
+				e.Body = map[string]interface{}{
 					"key": "val",
 					"nested": map[string]interface{}{
 						"nestedkey": "nestedval",
@@ -81,19 +133,25 @@ func TestCopyGoldenConfig(t *testing.T) {
 				}
 				e.Attributes = map[string]string{"key2": "val"}
 				return e
-			}(),
+			},
 		},
 		{
-			"attribute_to_record",
+			"attribute_to_body",
 			false,
+			func() *CopyOperatorConfig {
+				cfg := defaultCfg()
+				cfg.From = entry.NewAttributeField("key")
+				cfg.To = entry.NewBodyField("key2")
+				return cfg
+			}(),
 			func() *entry.Entry {
 				e := newTestEntry()
 				e.Attributes = map[string]string{"key": "val"}
 				return e
-			}(),
+			},
 			func() *entry.Entry {
 				e := newTestEntry()
-				e.Record = map[string]interface{}{
+				e.Body = map[string]interface{}{
 					"key": "val",
 					"nested": map[string]interface{}{
 						"nestedkey": "nestedval",
@@ -102,109 +160,51 @@ func TestCopyGoldenConfig(t *testing.T) {
 				}
 				e.Attributes = map[string]string{"key": "val"}
 				return e
-			}(),
+			},
 		},
 		{
 			"attribute_to_resource",
 			false,
+			func() *CopyOperatorConfig {
+				cfg := defaultCfg()
+				cfg.From = entry.NewAttributeField("key")
+				cfg.To = entry.NewResourceField("key2")
+				return cfg
+			}(),
 			func() *entry.Entry {
 				e := newTestEntry()
 				e.Attributes = map[string]string{"key": "val"}
 				return e
-			}(),
+			},
 			func() *entry.Entry {
 				e := newTestEntry()
 				e.Attributes = map[string]string{"key": "val"}
 				e.Resource = map[string]string{"key2": "val"}
 				return e
-			}(),
+			},
 		},
 	}
 
 	for _, tc := range cases {
-		t.Run("yaml/"+tc.name, func(t *testing.T) {
-			cfgFromYaml, yamlErr := configFromFileViaYaml(path.Join(".", "testdata", fmt.Sprintf("%s.yaml", tc.name)))
+		t.Run("BuildAndProcess/"+tc.name, func(t *testing.T) {
+			cfgFromMapstructure := tc.op
+			cfgFromMapstructure.OutputIDs = []string{"fake"}
+			cfgFromMapstructure.OnError = "drop"
+			ops, err := cfgFromMapstructure.Build(testutil.NewBuildContext(t))
+			require.NoError(t, err)
+			op := ops[0]
+
+			copy := op.(*CopyOperator)
+			fake := testutil.NewFakeOutput(t)
+			copy.SetOutputs([]operator.Operator{fake})
+			val := tc.input()
+			err = copy.Process(context.Background(), val)
 			if tc.expectErr {
-				require.Error(t, yamlErr)
+				require.Error(t, err)
 			} else {
-				cfgFromYaml.OutputIDs = []string{"fake"}
-				ops, err := cfgFromYaml.Build(testutil.NewBuildContext(t))
 				require.NoError(t, err)
-				op := ops[0]
-
-				remove := op.(*CopyOperator)
-				fake := testutil.NewFakeOutput(t)
-				remove.SetOutputs([]operator.Operator{fake})
-
-				err = remove.Process(context.Background(), tc.input)
-				if tc.expectErr {
-					require.Error(t, err)
-				}
-				require.NoError(t, err)
-
-				fake.ExpectEntry(t, tc.output)
-			}
-		})
-		t.Run("mapstructure/"+tc.name, func(t *testing.T) {
-			cfgFromMapstructure := defaultCfg()
-			mapErr := configFromFileViaMapstructure(
-				path.Join(".", "testdata", fmt.Sprintf("%s.yaml", tc.name)),
-				cfgFromMapstructure,
-			)
-			if tc.expectErr {
-				require.Error(t, mapErr)
-			} else {
-				cfgFromMapstructure.OutputIDs = []string{"fake"}
-				ops, err := cfgFromMapstructure.Build(testutil.NewBuildContext(t))
-				require.NoError(t, err)
-				op := ops[0]
-
-				remove := op.(*CopyOperator)
-				fake := testutil.NewFakeOutput(t)
-				remove.SetOutputs([]operator.Operator{fake})
-
-				err = remove.Process(context.Background(), tc.input)
-				if tc.expectErr {
-					require.Error(t, err)
-				}
-				require.NoError(t, err)
-
-				fake.ExpectEntry(t, tc.output)
+				fake.ExpectEntry(t, tc.output())
 			}
 		})
 	}
-}
-
-func configFromFileViaYaml(file string) (*CopyOperatorConfig, error) {
-	bytes, err := ioutil.ReadFile(file)
-	if err != nil {
-		return nil, fmt.Errorf("could not find config file: %s", err)
-	}
-
-	config := defaultCfg()
-	if err := yaml.Unmarshal(bytes, config); err != nil {
-		return nil, fmt.Errorf("failed to read config file as yaml: %s", err)
-	}
-
-	return config, nil
-}
-
-func configFromFileViaMapstructure(file string, result *CopyOperatorConfig) error {
-	bytes, err := ioutil.ReadFile(file)
-	if err != nil {
-		return fmt.Errorf("could not find config file: %s", err)
-	}
-
-	raw := map[string]interface{}{}
-
-	if err := yaml.Unmarshal(bytes, raw); err != nil {
-		return fmt.Errorf("failed to read data from yaml: %s", err)
-	}
-
-	err = helper.UnmarshalMapstructure(raw, result)
-	return err
-}
-
-func defaultCfg() *CopyOperatorConfig {
-	return NewCopyOperatorConfig("copy")
 }
