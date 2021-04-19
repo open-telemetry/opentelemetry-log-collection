b568990fb6d721b64ac9374be17b82e98611fd8e
Mrod1598 <miguelrodriguez1598@gmail.com>
2021-04-19 14:03:34 -0400

    Update all Config Tests

--- operator/builtin/transformer/add/config_test.go
+++ operator/builtin/transformer/add/config_test.go
@@ -91,7 +91,7 @@ func TestGoldenConfig(t *testing.T) {
 	}
 	for _, tc := range cases {
 		t.Run(tc.Name, func(t *testing.T) {
-			operatortest.RunGoldenConfigTest(defaultCfg(), t, tc)
+			operatortest.RunGoldenConfigTest(t, defaultCfg(), tc)
 		})
 	}
 }
