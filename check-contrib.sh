9bd426cea5e0a99da575814a7cf507244fbeab88
Prasad Katti <prasadmkatti@gmail.com>
2021-08-04 00:26:51 -0700

    Add workflow for testing contrib repo
    Fixes #199

---
+++ check-contrib.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+CURDIR=$(pwd)
+CONTRIB_PATH="/tmp/opentelemetry-collector-contrib"
+LOG_COLLECTION_MOD_NAME="github.com/open-telemetry/opentelemetry-log-collection"
+LOG_COLLECTION_MODULES=$(find ${CONTRIB_PATH} -type f -name "go.mod" -exec grep -l ${LOG_COLLECTION_MOD_NAME} {} \; | xargs -L 1 dirname | sort)
+echo "Log collection modules - ${LOG_COLLECTION_MODULES}"
+
+for module in ${LOG_COLLECTION_MODULES}
+do
+  pushd ${module}
+  go mod edit -replace github.com/open-telemetry/opentelemetry-log-collection=${CURDIR}
+  rm -fr go.sum
+  go mod tidy
+  go test
+  go mod edit -dropreplace github.com/open-telemetry/opentelemetry-log-collection
+  popd
+done
