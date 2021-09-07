0770d5f71d06086a5269a504257aa1fcb97ef0c5
Prasad Katti <prasadmkatti@gmail.com>
2021-09-07 15:56:38 -0700

    Ignore top level module and add -compat to go mod tidy

--- check-contrib.sh
+++ check-contrib.sh
@@ -5,7 +5,7 @@ set -e
 CURDIR=$(pwd)
 CONTRIB_PATH="/tmp/opentelemetry-collector-contrib"
 LOG_COLLECTION_MOD_NAME="github.com/open-telemetry/opentelemetry-log-collection"
-LOG_COLLECTION_MODULES=$(find ${CONTRIB_PATH} -type f -name "go.mod" -exec grep -l ${LOG_COLLECTION_MOD_NAME} {} \; | xargs -L 1 dirname | sort)
+LOG_COLLECTION_MODULES=$(find ${CONTRIB_PATH} ! -path ${CONTRIB_PATH}/go.mod ! -path ${CONTRIB_PATH}/cmd/configschema/go.mod -type f -name "go.mod" -exec grep -l ${LOG_COLLECTION_MOD_NAME} {} \; | xargs -L 1 dirname | sort)
 echo "Log collection modules - ${LOG_COLLECTION_MODULES}"
 
 for module in ${LOG_COLLECTION_MODULES}
@@ -13,7 +13,7 @@ do
   pushd ${module}
   go mod edit -replace github.com/open-telemetry/opentelemetry-log-collection=${CURDIR}
   rm -fr go.sum
-  go mod tidy
+  go mod tidy -compat=1.17
   go test
   go mod edit -dropreplace github.com/open-telemetry/opentelemetry-log-collection
   popd
