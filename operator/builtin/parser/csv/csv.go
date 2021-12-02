560c9fc9519315e4844bbdfe6bd38e2d1d20616c
Corbin Phelps <corbin.phelps@bluemedora.com>
2021-12-02 12:31:34 -0500

    Removed fmt.Errorf in csv for errors that did not require formatting
    Signed-off-by: Corbin Phelps <corbin.phelps@bluemedora.com>

--- operator/builtin/parser/csv/csv.go
+++ operator/builtin/parser/csv/csv.go
@@ -16,6 +16,7 @@ package csv
 import (
 	"context"
 	csvparser "encoding/csv"
+	"errors"
 	"fmt"
 	"strings"
 
@@ -51,7 +52,7 @@ func (c CSVParserConfig) Build(context operator.BuildContext) ([]operator.Operat
 	}
 
 	if c.Header == "" {
-		return nil, fmt.Errorf("missing required field 'header'")
+		return nil, errors.New("missing required field 'header'")
 	}
 
 	if c.FieldDelimiter == "" {
@@ -65,7 +66,7 @@ func (c CSVParserConfig) Build(context operator.BuildContext) ([]operator.Operat
 	fieldDelimiter := []rune(c.FieldDelimiter)[0]
 
 	if !strings.Contains(c.Header, c.FieldDelimiter) {
-		return nil, fmt.Errorf("missing field delimiter in header")
+		return nil, errors.New("missing field delimiter in header")
 	}
 
 	numFields := len(strings.Split(c.Header, c.FieldDelimiter))
